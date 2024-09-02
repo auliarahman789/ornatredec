ea5a48f80416c33735a7b5450024cde9e4395170
Saputra-prog <m4t11928@gmail.com>
2024-09-02 16:26:09 +0700

    tambah image halaman home page

--- src/app/components/homepage/edukasi.tsx
+++ src/app/components/homepage/edukasi.tsx
@@ -7,13 +7,16 @@ function Edukasi() {
       className="bg-cover bg-center min-h-screen"
       style={{ backgroundImage: "url('/img/edukasi hal.png')" }}
     >
-      <div className="pt-4">
+      <div className="grid grid-cols-2 pl-28 pt-20">
         <Image
           src="/img/edukasi buku gambar.png"
-          width={500}
-          height={500}
+          width={600}
+          height={600}
           alt="gambar"
         />
+        <div className="p-12">
+          <h1 className="text-6xl">Edukasi</h1>
+        </div>
       </div>
     </main>
   );
