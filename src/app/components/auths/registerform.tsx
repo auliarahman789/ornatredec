7e5dc250c3159b8cda24bedd7b6fccca9ea0b2a2
inibeliaa <beliaseptiramadhaani@gmail.com>
2024-09-02 22:36:57 +0700

    halaman register masih statis baru designnya

--- src/app/components/auths/registerform.tsx
+++ src/app/components/auths/registerform.tsx
@@ -1,21 +1,39 @@
+import Image from 'next/image'
 import React from 'react'
-import { Josefin_Sans } from 'next/font/google'
-
-const josefin = Josefin_Sans({subsets: ['latin']})
+import user from '../../../../public/icon/User_fill.svg'
+import email from '../../../../public/icon/Message_alt_fill.svg'
+import password from '../../../../public/icon/Lock_alt_fill.svg'
+import phone from '../../../../public/icon/Phone_fill.svg'
 
 function Register() {
   return (
     <main>
-        <div className='bg-white w-[500px] h-[480px]'>
+        <div className='bg-white w-[550px] h-[520px]'>
             <div className='flex flex-col justify-center items-center'>
-            <h1 className={`font-bold text-[28px] pt-[60px] text-[#3F9272] ${josefin}`}>Buat Akun</h1>
+            <h1 className='font-bold text-[30px] pt-[60px] text-[#3F9272]'>Buat Akun</h1>
             <form className='pt-[25px] gap-3.5 flex flex-col justify-center items-center'>
-              <div><input type='text' name='username' placeholder='Username' className='w-[270px] bg-[#E3FFF3] p-2 placeholder:text-[#3F9272] placeholder:text-[12px] text-[#3F9272]'/></div>
-              <div><input type='email' name='email' placeholder='Email' className='w-[270px] bg-[#E3FFF3] p-2 placeholder:text-[#3F9272] placeholder:text-[12px] text-[#3F9272]'/></div>
-              <div><input type='password' name='password' placeholder='Password' className='w-[270px] bg-[#E3FFF3] p-2 placeholder:text-[#3F9272] placeholder:text-[12px] text-[#3F9272]'/></div>
-              <div><input type='tel' name='telephone' placeholder='No Telephone' className='w-[270px] bg-[#E3FFF3] p-2 placeholder:text-[#3F9272] placeholder:text-[12px] text-[#3F9272]'/></div>
+            <div className='relative'>
+            <input type='text' name='username' placeholder='Username' className='w-[320px] pl-[50px] bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
+              <Image src={user} alt="username" width={25} height={25} 
+               className="absolute top-1/2 left-4 -translate-y-1/2"/>
+            </div>
+            <div className='relative'>
+              <input type='email' name='email' placeholder='Email' className='w-[320px] pl-[50px] bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
+              <Image src={email} alt="email" width={27} height={27} 
+               className="absolute top-1/2 left-4 -translate-y-1/2"/>
+            </div>
+            <div className='relative'>
+              <input type='password' name='password' placeholder='Password' className='w-[320px] pl-[50px] bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
+              <Image src={password} alt="password" width={27} height={27} 
+               className="absolute top-1/2 left-4 -translate-y-1/2"/>
+            </div>
+            <div className='relative'>
+              <input type='tel' name='telephone' placeholder='No Telephone' className='w-[320px] pl-[50px] bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
+              <Image src={phone} alt="telepon" width={27} height={27} 
+               className="absolute top-1/2 left-4 -translate-y-1/2"/>
+            </div>
             </form>
-            <button className='bg-[#3F9272] text-xs text-white px-12 py-[10px] mt-[40px] rounded-full'>
+            <button className='bg-[#3F9272] text-sm text-white px-12 py-[10px] mt-[30px] rounded-full'>
               Buat
             </button>
             </div>
--- src/app/components/auths/registerleft.tsx
+++ src/app/components/auths/registerleft.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 
 function RegisterL() {
   return (
-    <div
-    className="h-[480px] w-[400px] bg-no-repeat bg-center"
-    style={{
-      backgroundImage: "url('/img/registerleft.png')",
-      backgroundSize: "100% 100%",
-    }}
-  ></div>
+    <div className="h-[520px] w-[420px] bg-no-repeat bg-center text-white
+     flex flex-col items-center justify-center gap-5"
+      style={{
+        backgroundImage: "url('/img/registerleft.png')",
+        backgroundSize: "100% 100%",
+      }}
+    >
+      <div className='text-center'>
+        <h1 className='text-[30px] font-bold'>Selamat Datang</h1>
+        <p>Tap disini jika kamu sudah<br/> 
+        memiliki akun di T-trana</p>
+      </div>
+      <button className='bg-transparent rounded-full h-[45px] w-[160px] border-[1px] border-white'>
+        Login
+      </button>
+  </div>
   
 
   
