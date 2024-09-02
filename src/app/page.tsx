276b213aa29f1d755e6328519a0f181b30e5bdf7
RioRivaldi <rivaldirio090@gmail.com>
2024-09-02 15:34:03 +0700

    mencoba halaman login belom (fiks)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Footer from "./components/homepage/footer";
 import Navbar from "./components/homepage/navbar";
+import Login from "./components/auths/login";
 
 function page() {
   return (
     <div>
       <Navbar />
       <Footer />
+      <Login/>
     </div>
   );
 }
