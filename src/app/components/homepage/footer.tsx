3111f57482fc93412ba516ee7b583329f29e8faf
KevinWijaya16 <kevinwijaya161207@gmail.com>
2024-09-03 15:37:37 +0700

    mengerjakan footer belum beres

--- src/app/components/homepage/footer.tsx
+++ src/app/components/homepage/footer.tsx
@@ -7,16 +7,18 @@ function Footer() {
         <div className="my-[90px]">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 translate-y-10">
             <div className="flex flex-col items-center md:items-start ms-[141px]">
-              <span className="text-gray-400 bg-green-400 leading-tight max-w-[390px]">
+              <span className="text-gray-400 leading-tight max-w-[360px]">
                 Ornatredec adalah web yang meyediakan fitur seperti forum untuk
                 penggemar, edukasi tentang tanaman hias,burung hias dan ikan
                 hias juga menyediakan laman untuk jual beli
               </span>
             </div>
-            <div className="bg-orange-500 space-x-12 flex justify-center">
-              <span>About</span>
-              <span>Kontak</span>
-              <span>Sosial Media</span>
+            <div className="bg-orange-500 space-x-[77px] flex justify-center -mt-32 mr-10">
+              <span className="bg-slate-500 px-4 py-2 mb-auto">About</span>
+              <span className="bg-slate-600 px-4 py-2 mb-auto">Kontak</span>
+              <span className="bg-slate-800 px-4 py-2 mb-auto">
+                Sosial Media
+              </span>
             </div>
           </div>
         </div>
