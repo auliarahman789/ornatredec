1e18eceabac7f9d231f896b2497838362233bafc
inibeliaa <beliaseptiramadhaani@gmail.com>
2024-09-03 10:38:49 +0700

    ngebenrin register

--- src/app/components/auths/registerform.tsx
+++ src/app/components/auths/registerform.tsx
@@ -8,27 +8,27 @@ import phone from '../../../../public/icon/Phone_fill.svg'
 function Register() {
   return (
     <main>
-        <div className='bg-white w-[550px] h-[520px]'>
+        <div className='bg-white w-[530px] h-[520px]'>
             <div className='flex flex-col justify-center items-center'>
             <h1 className='font-bold text-[30px] pt-[60px] text-[#3F9272]'>Buat Akun</h1>
-            <form className='pt-[25px] gap-3.5 flex flex-col justify-center items-center'>
+            <form action='' className='pt-[25px] gap-3.5 flex flex-col justify-center items-center'>
             <div className='relative'>
-            <input type='text' name='username' placeholder='Username' className='w-[320px] pl-[50px] bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
+            <input type='text' name='username' placeholder='Username' className='w-[320px] pl-14 bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
               <Image src={user} alt="username" width={25} height={25} 
                className="absolute top-1/2 left-4 -translate-y-1/2"/>
             </div>
             <div className='relative'>
-              <input type='email' name='email' placeholder='Email' className='w-[320px] pl-[50px] bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
+              <input type='email' name='email' placeholder='Email' className='w-[320px] pl-14 bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
               <Image src={email} alt="email" width={27} height={27} 
                className="absolute top-1/2 left-4 -translate-y-1/2"/>
             </div>
             <div className='relative'>
-              <input type='password' name='password' placeholder='Password' className='w-[320px] pl-[50px] bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
+              <input type='password' name='password' placeholder='Password' className='w-[320px] pl-14 bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
               <Image src={password} alt="password" width={27} height={27} 
                className="absolute top-1/2 left-4 -translate-y-1/2"/>
             </div>
             <div className='relative'>
-              <input type='tel' name='telephone' placeholder='No Telephone' className='w-[320px] pl-[50px] bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
+              <input type='tel' name='telephone' placeholder='No Telephone' className='w-[320px] pl-14 bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md' />
               <Image src={phone} alt="telepon" width={27} height={27} 
                className="absolute top-1/2 left-4 -translate-y-1/2"/>
             </div>
