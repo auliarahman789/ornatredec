daaef0a300fd1fa6a65b897f69e588f5742bb141
KevinWijaya16 <kevinwijaya161207@gmail.com>
2024-09-09 11:44:24 +0700

    mengubah text main

--- src/app/components/Edukasi/main.tsx
+++ src/app/components/Edukasi/main.tsx
@@ -4,7 +4,7 @@ function Main() {
   return (
     <div className="flex pb-10 bg-slate-300">
       <div className="bg-white mt-[40px] mb-[] mx-[100px] w-[1100px] h-[415px]">
-        paling atas
+        paling atass
       </div>
     </div>
   );
