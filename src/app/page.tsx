0b6854a76f3db8abf0f6e9c03345d117dfd2b375
Saputra-prog <m4t11928@gmail.com>
2024-09-09 12:00:15 +0700

    tes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Footer from "./components/homepage/footer";
-import Navbar from "./components/homepage/navbar";
 import Home from "./components/homepage/home";
 import Edukasi from "./components/homepage/edukasi";
-import Login from "./components/auths/loginform";
 import Produk from "./components/homepage/produk";
 import Forum from "./components/homepage/forum";
 
@@ -16,7 +14,6 @@ function page() {
       <main className=" min-h-screen w-full ">
         <Forum />
       </main>
-
       <main className="flex min-h-[30%] w-full px-[8%]">
         <Footer />
       </main>
