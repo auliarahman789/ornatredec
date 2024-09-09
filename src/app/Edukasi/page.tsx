4945fcb2261d23bce8df94561b55fcd6d8ee7261
KevinWijaya16 <kevinwijaya161207@gmail.com>
2024-09-09 11:09:42 +0700

    membuat folder dan file untuk laman edukasi

---
+++ src/app/Edukasi/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Edukasi() {
+  return <div></div>;
+}
+
+export default Edukasi;
---
+++ src/app/components/Edukasi/burung.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Burung() {
+  return <div></div>;
+}
+
+export default Burung;
