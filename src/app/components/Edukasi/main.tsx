89f709fdc25cdc5f594351f05e071e46c336b02a
KevinWijaya16 <kevinwijaya161207@gmail.com>
2024-09-09 11:35:52 +0700

    perubahan ukuran div bagian atas

--- src/app/components/Edukasi/main.tsx
+++ src/app/components/Edukasi/main.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Main() {
   return (
     <div className="flex pb-10 bg-slate-300">
-      <div className="bg-white mt-[40px] mb-[] mx-[115px] w-[1024px] h-[415px]">
+      <div className="bg-white mt-[40px] mb-[] mx-[100px] w-[1100px] h-[415px]">
         paling atas
       </div>
     </div>
