ac9643fd34a457af18e43745c37fb522d4f13b11
Saputra-prog <m4t11928@gmail.com>
2024-09-09 19:46:55 +0700

    update edukasi halaman

--- src/app/Edukasi/page.tsx
+++ src/app/Edukasi/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Main from "../components/Edukasi/main";
+import Tanaman from "../components/Edukasi/tanaman";
 
 function Edukasi() {
   return (
-    <div className="bg-[#E4FFF2] min-h-screen">
+    <div className="bg-[#E4FFF2]">
       <Main />
+      <Tanaman />
     </div>
   );
 }
