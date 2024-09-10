f0ab858dc601103cf95209f415470ca7b3c65856
RioRivaldi <rivaldirio090@gmail.com>
2024-09-10 10:06:12 +0700

    merapihkan login

--- src/app/components/auths/loginform.tsx
+++ src/app/components/auths/loginform.tsx
@@ -53,7 +53,7 @@ const Login = () => {
                 name="username"
                 onChange={(e) => setUsername(e.target.value)}
                 placeholder="Username"
-                className="w-[320px] pl-16 bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8/ text-[#3F9272] rounded-md"
+                className="w-[320px] pl-14 bg-[#E3FFF3] pb-[13px] pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8/ text-[#3F9272] rounded-md"
               />
               <Image
                 src={user}
@@ -69,7 +69,7 @@ const Login = () => {
                 name="password"
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
-                className="w-[320px] pl-16 bg-[#E3FFF3] pb-[13px] mt-6 pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8 text-[#3F9272] rounded-md"
+                className="w-[320px] pl-14 bg-[#E3FFF3] pb-[13px] mt-6 pt-[15px] placeholder:text-[#3F9272] placeholder:text-[18px] text-[19px] placeholder:font-light ps-8/ text-[#3F9272] rounded-md"
               />
               <Image
                 src={passwordIcon}
