8945144b08f9e41ad96c205b6f8ff9a4693e28b7
Saputra-prog <m4t11928@gmail.com>
2024-09-10 11:48:13 +0700

    membuat halaman edukasi tanaman

--- src/app/components/Edukasi/ikan.tsx
+++ src/app/components/Edukasi/ikan.tsx
@@ -2,21 +2,23 @@ import React from "react";
 
 function Ikan() {
   return (
-    <div className="grid gap-0 grid-cols-2 mx-[100px] pb-[8%]" id="ikan">
-      <div className="translate-x-[13px]">
-        <h1 className="text-[#3F9272] font-bold text-[40px]">
-          Apa Itu Ikan Hias?
-        </h1>
-        <p className="text-[20px] me-[4%] mt-[2%] text-[#54A082]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-          do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-          ad minim ve
-        </p>
+    <div id="ikan">
+      <div className="grid gap-0 grid-cols-2 mx-[100px] pb-[8%]" id="ikan">
+        <div className="translate-x-[13px]">
+          <h1 className="text-[#3F9272] font-bold text-[40px]">
+            Apa Itu Ikan Hias?
+          </h1>
+          <p className="text-[20px] me-[4%] mt-[2%] text-[#54A082]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim ve
+          </p>
+        </div>
       </div>
     </div>
   );
