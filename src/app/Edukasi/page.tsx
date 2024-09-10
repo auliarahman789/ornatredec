5fa9999c4833018310d17041e3af6295a781f757
KevinWijaya16 <kevinwijaya161207@gmail.com>
2024-09-10 10:37:59 +0700

    edukasi

--- src/app/Edukasi/page.tsx
+++ src/app/Edukasi/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Main from "../components/(Edukasi)/main";
-import Footer from "../components/homepage/footer";
+import Main from "../components/Edukasi/main";
 import Tanaman from "../components/Edukasi/tanaman";
 
 function Edukasi() {
