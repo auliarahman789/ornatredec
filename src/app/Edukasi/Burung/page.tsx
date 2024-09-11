081a5869334569f8c74c1647f512e81d692f8662
inibeliaa <beliaseptiramadhaani@gmail.com>
2024-09-11 10:59:15 +0700

    manggil burung

--- src/app/Edukasi/Burung/page.tsx
+++ src/app/Edukasi/Burung/page.tsx
@@ -1,11 +1,12 @@
-import Burung from "../components/Edukasi/burung";
-import Footer from "../components/homepage/footer";
+import Burung from "@/app/components/Edukasi/burung";
+import Burung2 from "@/app/components/Edukasi/burung2";
+import Footer from "@/app/components/homepage/footer";
 
 function Edukasi() {
   return (
     <div>
       <div className="bg-[#E4FFF2] min-h-screen">
-        <Tanaman />
+        <Burung />
         <Footer />
       </div>
     </div>
