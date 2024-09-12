7aa01fee5e46934d1b60ab1af28515c5c2098768
inibeliaa <beliaseptiramadhaani@gmail.com>
2024-09-12 13:27:45 +0700

    nambahin navigasi dari edukasi ke tumbuhan dll

--- src/app/Edukasi/page.tsx
+++ src/app/Edukasi/page.tsx
@@ -1,8 +1,16 @@
+"use client"
 import React from "react";
 import Image from "next/image";
 import Footer from "../components/homepage/footer";
+import Link from "next/link";
 
-function page() {
+const page = ({
+  isAnimated,
+  setIsAnimated,
+}: {
+  isAnimated: any;
+  setIsAnimated: any;
+}) => {
   return (
     <><div className="overflow-hidden bg-[#E4FFF2] pb-40">
       <div className="bg-white">
@@ -46,7 +54,9 @@ function page() {
                 tanaman hias di sini, dan tingkatkan pengalaman Anda dalam
                 menjaga keindahan dan kesehatan koleksi Anda.
               </p>
-              <p className="translate-x-[63%] mt-5">Lihat selengkapnya</p>
+              <Link href='/Edukasi/Tumbuhan#tumbuhan' onClick={(e) => {setIsAnimated(false);}}>
+                <p className="translate-x-[63%] mt-14">Lihat selengkapnya</p>
+              </Link>
             </div>
           </div>
           <div className="bg-white w-[80%] h-[338px] mx-auto mt-9  shadow-[3px_5px_4px] shadow-[#0000002e]">
@@ -66,7 +76,9 @@ function page() {
                 hias di sini, dan tingkatkan pengalaman Anda dalam menjaga
                 keindahan dan kesehatan koleksi Anda.
               </p>
-              <p className="translate-x-[63%] mt-14">Lihat selengkapnya</p>
+              <Link href='/Edukasi/Ikan#ikan' onClick={(e) => {setIsAnimated(false);}}>
+                <p className="translate-x-[63%] mt-14">Lihat selengkapnya</p>
+              </Link>
             </div>
           </div>
           <div className="bg-white w-[80%] h-[338px] mx-auto mt-9  shadow-[3px_5px_4px] shadow-[#0000002e]">
@@ -87,7 +99,10 @@ function page() {
                 hias di sini, dan tingkatkan pengalaman Anda dalam menjaga
                 keindahan dan kesehatan koleksi Anda.
               </p>
-              <p className="translate-x-[63%] mt-14">Lihat selengkapnya</p>
+              <Link href='/Edukasi/Burung#burung' onClick={(e) => {setIsAnimated(false);}}>
+                <p className="translate-x-[63%] mt-14">Lihat selengkapnya</p>
+              </Link>
+              
             </div>
           </div>
         </div>
