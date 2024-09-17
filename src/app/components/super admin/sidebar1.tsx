7099b65e91823efeb690999421a0de6dc3e81e34
inibeliaa <beliaseptiramadhaani@gmail.com>
2024-09-17 16:53:47 +0700

    ngedit sidebar nambahin animasi

--- src/app/components/super admin/sidebar1.tsx
+++ src/app/components/super admin/sidebar1.tsx
@@ -10,22 +10,22 @@ import forumm from '../../../../public/icon/comment_fill.svg'
 function SidebarSp() {
   return (
     <div className='fixed top-0 left-0'>
-      <nav className='bg-gradient-to-b from-[#167960] to-[#28DFB1] w-64 h-screen flex flex-col'>
+      <nav className='nav bg-gradient-to-b from-[#167960] to-[#28DFB1] w-64 h-screen flex flex-col'>
         <Image src={logo} className='w-[40%] mt-4 mx-auto h-[6%]' width={200} height={100} alt='logo'/>
-        <ul className='text-[20px] mt-[25%] font-bold text-white space-y-12 '>
-          <li className='cursor-pointer flex bg-white py-3 ms-[10%] rounded-s-2xl'>
-            <Image src={dashboardd} className='w-8 h-8 me-5 ms-3' width={30} height={30} alt='dashboard' />
-            <span className='mt-1 text-[#308967]'>Dashboard</span></li>
-            <li className='cursor-pointer flex ms-[15%]'>
+        <ul className='text-[20px] mt-[25%] font-bold text-white space-y-12 ms-[15%]'>
+          <li className='cursor-pointer flex'>
+            <Image src={dashboardd} className='w-8 h-8 me-5' width={30} height={30} alt='dashboard' />
+            <span className='mt-1'>Dashboard</span></li>
+            <li className='cursor-pointer flex '>
             <Image src={produkk} className='w-5 ms-1 h-6 me-6' width={30} height={30} alt='dashboard' />
             Produk</li>
-            <li className='cursor-pointer flex ms-[15%]'>
+            <li className='cursor-pointer flex '>
             <Image src={statistikk} className='w-6 h-6 me-5' width={30} height={30} alt='dashboard' />
             Statistik</li>
-            <li className='cursor-pointer flex ms-[15%]'>
+            <li className='cursor-pointer flex '>
             <Image src={edukasii} className='w-6 h-6 me-5' width={30} height={30} alt='dashboard' />
             Edukasi</li>
-            <li className='cursor-pointer flex ms-[15%]'>
+            <li className='cursor-pointer flex '>
             <Image src={forumm} className='w-6 h-6 me-5' width={30} height={30} alt='dashboard' />
             Forum</li>
         </ul>
