ef1f537e58d85c149f563b87f8c205a14ffe4a5c
KevinWijaya16 <kevinwijaya161207@gmail.com>
2024-09-20 10:35:06 +0700

    menganti nama diagram dengan nama bulan

--- src/app/diagram/batang/page.tsx
+++ src/app/diagram/batang/page.tsx
@@ -13,46 +13,46 @@ import {
 
 const data = [
   {
-    name: "Page A",
+    name: "Januari",
     uv: 4000,
     pv: 2400,
-    amt: 2400,
+    amt: 300,
   },
   {
-    name: "Page B",
+    name: "Februari",
     uv: 3000,
     pv: 1398,
-    amt: 2210,
+    amt: 250,
   },
   {
-    name: "Page C",
+    name: "Maret",
     uv: 2000,
     pv: 9800,
-    amt: 2290,
+    amt: 200,
   },
   {
-    name: "Page D",
+    name: "April",
     uv: 2780,
     pv: 3908,
-    amt: 2000,
+    amt: 150,
   },
   {
-    name: "Page E",
+    name: "Mei",
     uv: 1890,
     pv: 4800,
-    amt: 2181,
+    amt: 100,
   },
   {
-    name: "Page F",
+    name: "Juni",
     uv: 2390,
     pv: 3800,
-    amt: 2500,
+    amt: 50,
   },
   {
-    name: "Page G",
+    name: "Juli",
     uv: 3490,
     pv: 4300,
-    amt: 2100,
+    amt: 0,
   },
 ];
 
@@ -62,29 +62,36 @@ export default class Example extends PureComponent {
 
   render() {
     return (
-      <ResponsiveContainer width="100%" height="50%">
+      <ResponsiveContainer width="100%" height="90%">
         <BarChart
           width={500}
-          height={300}
+          height={500}
           data={data}
           margin={{
-            top: 5,
-            right: 30,
-            left: 20,
+            top: 50,
+            right: 40,
+            left: 10,
             bottom: 5,
           }}
-          barSize={20}
+          barSize={40}
         >
           <XAxis
             dataKey="name"
             scale="point"
-            padding={{ left: 10, right: 10 }}
+            padding={{ left: 50, right: 10 }}
           />
           <YAxis />
           <Tooltip />
           <Legend />
           <CartesianGrid strokeDasharray="3 3" />
-          <Bar dataKey="pv" fill="#8884d8" background={{ fill: "#eee" }} />
+          <Bar
+            dataKey="pv"
+            fill="#44FFC7"
+            background={{ fill: "#eee" }}
+            style={{
+              filter: "drop-shadow(5px 5px 10px rgba(0, 0, 0, 0.3))",
+            }}
+          />
         </BarChart>
       </ResponsiveContainer>
     );
