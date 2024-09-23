5c353572a06251ceee4616f864e16131318263fa
KevinWijaya16 <kevinwijaya161207@gmail.com>
2024-09-23 10:40:21 +0700

    layout

--- src/app/components/layout2.tsx
+++ src/app/components/layout2.tsx
@@ -2,6 +2,7 @@
 import { Josefin_Sans as JosefinSans } from "next/font/google";
 import { usePathname } from "next/navigation";
 import Navbar from "./homepage/navbar";
+import SidebarSp from "./super admin/sidebar1";
 
 const josefinSans = JosefinSans({ subsets: ["latin"] });
 
@@ -11,17 +12,20 @@ const disableNavbar = [
   "/profile/post",
   "/profile/edit",
   "/profile/riwayat",
-<<<<<<< HEAD
-=======
   "/Superadmin",
   "/Superadmin/Dashboard",
   "/Superadmin/Produk",
   "/Superadmin/Akun",
   "/Superadmin/Statistik",
   "/Superadmin/Forum",
->>>>>>> a31ae0d1fb9484a9403db79e9a8c8b88d9b3246f
 ];
-
+const superNavbar = [
+  "/Superadmin",
+  "/Superadmin/Produk",
+  "/Superadmin/Akun",
+  "/Superadmin/Statistik",
+  "/Superadmin/Forum",
+];
 export default function RootLayoutClient({
   children,
 }: Readonly<{
@@ -29,11 +33,11 @@ export default function RootLayoutClient({
 }>) {
   const pathname = usePathname();
   const isNavbarDisabled = disableNavbar.includes(pathname);
-
+  const isSuperNavbar = superNavbar.includes(pathname);
   return (
     <div className={josefinSans.className}>
       {!isNavbarDisabled && <Navbar />}
-
+      {isSuperNavbar && <SidebarSp />}
       {children}
     </div>
   );
