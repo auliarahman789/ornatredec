8e3ff37215e3633b55e0365e2b043d6a481be595
KevinWijaya16 <kevinwijaya161207@gmail.com>
2024-09-25 20:29:36 +0700

    menambahkan text online dan offline

--- src/app/Superadmin/Statistik/page.tsx
+++ src/app/Superadmin/Statistik/page.tsx
@@ -15,9 +15,17 @@ function Page() {
           <BarKecilStatistik />
           <PieChartStatistik />
         </div>
-        <div className="flex ms-[35%]">
-          <p className="bg-slate-400">Online</p>
-          <p className="bg-red-500">Offline</p>
+        <div className="flex ms-[34%] space-x-10 -translate-y-7 text-sm">
+          <p className="flex text-white items-center space-x-2 gap-x-2">
+            <span className="w-3 h-3 bg-[#6BF8C2] rounded-full inline-block"></span>
+            {/* Lingkaran kecil putih */}
+            Online
+          </p>
+          <p className="flex text-white items-center space-x-2 gap-x-2">
+            <span className="w-3 h-3 bg-white rounded-full inline-block"></span>
+            {/* Lingkaran kecil putih */}
+            Offline
+          </p>
         </div>
       </div>
     </div>
