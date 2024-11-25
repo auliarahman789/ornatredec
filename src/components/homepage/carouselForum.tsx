b4be0813c724dc314cb0d87db06658618e43ace1
inibeliaa <beliaseptiramadhaani@gmail.com>
2024-11-25 14:54:30 +0700

    membenarkan carousel

--- src/components/homepage/carouselForum.tsx
+++ src/components/homepage/carouselForum.tsx
@@ -65,7 +65,9 @@ export default function ForumCarousel() {
 
   return (
     <div className="w-[60%] p-[5%] h-[40%] justify-self-center">
-      <Slider ref={sliderRef} {...settings}>
+      
+      <Slider ref={sliderRef}
+        {...settings}>
         {data.map((item: any, id: number) => (
           <div className="p-6 w-[40%]" key={id}>
             <div className="h-[220px] mx-[3%] bg-white rounded-[34px]">
@@ -130,6 +132,3 @@ export default function ForumCarousel() {
   );
 }
 
-function setData(arg0: any) {
-  throw new Error("Function not implemented.");
-}
